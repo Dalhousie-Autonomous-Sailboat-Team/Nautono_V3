075bce277e2a92773ba10092ba9f47dabaf67e8d
zachf <zach.fraser88@gmail.com>
2025-03-15 12:09:04 -0300

    fix

---
+++ User/Inc/motor.h
@@ -0,0 +1,17 @@
+/** @file motor.h
+ *
+ * @brief Motor control functions.
+ *
+ * @par
+ * COPYRIGHT NOTICE: (c) 2025 DalMAST.  All rights reserved.
+ */
+
+#ifndef MOTOR_H
+#define MOTOR_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#endif /* MOTOR_H */
+
+/*** end of file ***/
---
+++ User/Src/motor.c
@@ -0,0 +1,15 @@
+/** @file motor.c
+ *
+ * @brief Tasks and functions for controlling motor position.
+ *
+ * @par
+ * COPYRIGHT NOTICE: (c) 2025 DalMAST. All rights reserved.
+ */
+
+#include "main.h"
+#include "cmsis_os2.h"
+#include "FreeRTOS.h"
+
+#include "motor.h"
+#include "debug.h"
+
